4e303c8afb1b3ad67707c8b7599167e89b510b91
Aldin Smajlovic <aldin.smajlovic@wisest.se>
2026-05-25 14:22:30 +0200

    Update profile to IT Security Consultant @ Wisest AB and add Security Policy & Hall of Fame

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Aldin Smajlovic';
-export const SITE_DESCRIPTION = 'Security Researcher, Pentester and Student at ITHs.';
+export const SITE_DESCRIPTION = 'IT Security Consultant @ Wisest AB. Security specialist and developer of internal security tools.';
